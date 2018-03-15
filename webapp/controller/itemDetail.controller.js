096206ba17d0f9f370ae2c735188b4b0e2d7073b
sriniui5 <srinivas.hogarehalli@gmail.com>
2018-03-15 06:55:15 +0000

    added new view and controller for item detail

---
+++ webapp/controller/itemDetail.controller.js
@@ -0,0 +1,41 @@
+/*global location */
+sap.ui.define([
+		"finalapp/controller/BaseController",
+		"sap/ui/model/json/JSONModel",
+		"finalapp/model/formatter"
+	], function (BaseController, JSONModel, formatter) {
+		"use strict";
+
+		return BaseController.extend("finalapp.controller.itemDetail", {
+
+			formatter: formatter,
+
+			/* =========================================================== */
+			/* lifecycle methods                                           */
+			/* =========================================================== */
+
+			onInit : function () {
+				// Model used to manipulate control states. The chosen values make sure,
+				// detail page is busy indication immediately so there is no break in
+				// between the busy indication for loading the view's meta data
+				var oViewModel = new JSONModel({
+					busy : false,
+					delay : 0,
+					lineItemListTitle : this.getResourceBundle().getText("detailLineItemTableHeading")
+				});
+
+				this.getRouter().getRoute("object").attachPatternMatched(this._onObjectMatched, this);
+
+				this.setModel(oViewModel, "detailView");
+
+				this.getOwnerComponent().getModel().metadataLoaded().then(this._onMetadataLoaded.bind(this));
+			}
+
+			/* =========================================================== */
+			/* event handlers                                              */
+
+		
+		});
+
+	}
+);
